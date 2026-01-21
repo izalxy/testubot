e969aea8c850f2b9fe781d16db76305a138712f8
zalst <izalw86@gmail.com>
2026-01-21 22:47:59 +0700

    Add fansx/modules/infobot.py via Telegram Bot

---
+++ fansx/modules/infobot.py
@@ -0,0 +1,10 @@
+import asyncio
+from pyrogram import Client, filters
+
+# Informasi modul
+__MODULE__ = "info bot"
+__HELP__ = """
+<b>IZAL USERBOT
+
+⎆ adalah user bot Izall jembot yang digunakan untuk memudahkan pengguna dalam pekerjaan mereka seperti broadcast otomatis ke semua grup , convert gambar menjadi link , serta banyak lagi kegunaan nya
+"""
